108ff017888d161399e43a9c355d2b1f0ef23270
jklim23 <junkang2233@gmail.com>
2023-10-24 09:21:03 +0800

    done qns 4b

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Mario/MarioHVector2D.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Mario/MarioHVector2D.cs
@@ -15,15 +15,33 @@ public class MarioHVector2D : MonoBehaviour
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void FixedUpdate()
     {
-        gravityDir = new HVector2D(planet.position - transform.position);  
-        moveDir = new HVector2D(gravityDir.y, -gravityDir.x);
 
-        // Your code here
-        // ...
+        gravityDir = new HVector2D(planet.position - transform.position);
+        //Debug.Log( " " + gravityDir.x+" "+ gravityDir.y);
+        moveDir = new HVector2D(gravityDir.y, -gravityDir.x);
+        //Debug.Log(moveDir.x + " " + moveDir.y);
+        moveDir.Normalize();
+        //Debug.Log(moveDir.x + " " + moveDir.y);
+        moveDir = moveDir * -1f;
+        //Debug.Log(moveDir.x + " " + moveDir.y);
+        rb.AddForce(moveDir.ToUnityVector2() * force);
+
+        gravityDir.Normalize();
+        gravityNorm = gravityDir;
+        //Debug.Log(gravityNorm.x +" "+gravityNorm.y + " " + gravityDir.x + " " + gravityDir.y);
+
+        rb.AddForce(gravityNorm.ToUnityVector2() * gravityStrength);
+
+        float angle = Vector3.SignedAngle(Vector3.right, moveDir.ToUnityVector2(), Vector3.forward);
+
+        rb.MoveRotation(Quaternion.Euler(0, 0, angle));
+
+        DebugExtension.DebugArrow(transform.position, gravityNorm.ToUnityVector2() * gravityStrength, Color.red);
     }
 }
