259f6983816ae3603d732e0e4a10a9f1cb01d24d
jklim23 <junkang2233@gmail.com>
2023-10-28 14:23:16 +0800

    did the angle tracking

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/Player.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/Player.cs
@@ -1,91 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public bool IsCaptain = true;
     public Player OtherPlayer;
-    private Transform PlayerTF, OtherPlayerTF;
+
     private void Start()
     {
-       PlayerTF = transform;
-       OtherPlayerTF = OtherPlayer.transform;
+        
+
+
+    }
+    private void Update()
+    {
+        //if (IsCaptain)
+        //{
+        //    Debug.Log(AngleToPlayer());
+        //}
+
     }
     void FixedUpdate()
     {
         if (IsCaptain)
         {
             //6b arrow
-            DebugExtension.DebugArrow(PlayerTF.position, OtherPlayerTF.position - PlayerTF.position, Color.black);
-            DebugExtension.DebugArrow(PlayerTF.position, transform.forward, Color.blue);
+            //DebugExtension.DebugArrow(transform.position, OtherPlayer.transform.position - transform.position, Color.black);
+            //DebugExtension.DebugArrow(transform.position, transform.forward, Color.blue);
 
             //float angle = // Your code here
             //Debug.Log(angle);
 
+            Debug.Log(AngleToPlayer());
 
 
         }
     }
 
-    //float Magnitude(Vector3 vector)
-    //{
-    //    // Your code here
-    //}
+    float Magnitude(Vector3 vector)
+    {
+         return (float)Math.Sqrt(vector.x * vector.x + vector.y * vector.y);
+    }
 
-    //Vector3 Normalise(Vector3 vector)
-    //{
-    //    // Your code here
-    //}
+    Vector3 Normalise(Vector3 vector)
+    {
+        return Vector3.Normalize(vector);
+    }
 
-    //float Dot(Vector3 vectorA, Vector3 vectorB)
-    //{
-    //    // Your code here
-    //}
+    float Dot(Vector3 vectorA, Vector3 vectorB)
+    {
+        return (vectorA.x * vectorB.x + vectorA.y * vectorB.y);
+    }
 
-    //float AngleToPlayer()
-    //{
-    //    // Steps to calculate the angle between the direction Captain is facing and 
-    //    // the direction from Captain to Other
-    //    // 1. A
-    //    // 2. B
-    //    // 3. Calculate the angle between the vectors from A and B
-    //    //    3.1 Calculate the dot product between A and B
-    //    //       3.1.1 Calculate the magnitude of the vector from Captain to Other (A)
-    //    //       3.1.2 Normalise the vector from Captain to Other
-    //    //       3.1.3 Calculate the dot product of the normalised vectors A and B
-    //    //    3.2 From the dot product, calculate the actual angle between A and B
-    //    //       3.2.1 Take the arc cosine (acos) of the dot product (because if 
-    //    //             both vector are normalised, the dot product equals the cos
-    //    //             of the angle between vectors A and B.
-    //    //       3.2.2 The acos angle returned is in radians. We must convert to
-    //    //             degrees.
+    float AngleToPlayer()
+    {
 
-    //    // A
-    //    // Steps to draw the arrow that represents the vector from Captain to Other 
-    //    // 1. Find the vector from Captain to Other
-    //    //     1.1 Find the position of Captain (A -- from)
-    //    //     1.2 Find the position of Other (B -- to)
-    //    //     1.3 Calculate B-A to get the vector from A to B
-    //    // 2. Draw the arrow to represent visually the vector AB
-    //    //
 
-    //    // Your code here
+        
 
-    //    // B
-    //    // Steps to draw an arrow that represents which direction Captain is facing
-    //    // 1. Find which vector to draw as an arrow
-    //    //    1.1 The transform.forward vector of Captain
-    //    // 2. Draw the arrow to represent visually the the transform.forward vector of Captain
-    //    DebugExtension.DebugArrow(transform.position, transform.forward, Color.blue);
+        float angle = math.acos(Dot(transform.position , OtherPlayer.transform.position)/
+        (Magnitude(transform.position) * Magnitude(OtherPlayer.transform.position))) * Mathf.Rad2Deg;
 
-    //    // CALCULATING THE ANGLE
-
-    //    // Your code here
-    //    // ...
-
-    //    // Your code here
-    //}
+        DebugExtension.DebugArrow(transform.position, OtherPlayer.transform.position - transform.position, Color.black);
+        DebugExtension.DebugArrow(transform.position, transform.forward, Color.blue);
+        return angle;
+    }
 
 
 }
