82ec1061f0d44d0fc43279a229c3f01389264082
jklim23 <junkang2233@gmail.com>
2023-10-29 16:58:03 +0800

    did worksheet to qns 6c part 2

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
@@ -2,34 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Unity.Mathematics;
+using System;
 
 public class SoccerPlayer : MonoBehaviour
 {
-    //public bool IsCaptain = false;
-    //public SoccerPlayer[] OtherPlayers;
-    //public float rotationSpeed = 1f;
+    public bool IsCaptain = false;
+    public SoccerPlayer[] OtherPlayers;
+    public float rotationSpeed = 1f;
 
-    //float angle = 0f;
+    float angle = 0f;
 
-    //private void Start()
-    //{
-       
-    //}
+    private void Start()
+    {
+        OtherPlayers = FindObjectsOfType<SoccerPlayer>().Where(t => t != this).ToArray();
+        //ForEachLoopToFindPlayers();
+    }
+    private void Update()
+    {
+        DebugExtension.DebugArrow(transform.position, transform.forward, Color.red);
 
-    //float Magnitude(Vector3 vector)
-    //{
+        if (IsCaptain)
+        {
+            angle += Input.GetAxis("Horizontal") * rotationSpeed;
+            transform.localRotation = Quaternion.AngleAxis(angle,Vector3.up);
+            Debug.DrawRay(transform.position,transform.forward * 10f,Color.red);
+            DrawVectors();
+        }
         
-    //}
+    }
 
-    //Vector3 Normalise(Vector3 vector)
-    //{
-        
-    //}
+    float Magnitude(Vector3 vector)
+    {
+        return (float)Math.Sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+    }
 
-    //float Dot(Vector3 vectorA, Vector3 vectorB)
-    //{
-        
-    //}
+    Vector3 Normalise(Vector3 vector)
+    {
+        float mag = Magnitude(vector);
+        vector.x /= mag;
+        vector.y /= mag;
+        vector.z /= mag;
+        return vector;
+    }
+
+    float Dot(Vector3 vectorA, Vector3 vectorB)
+    {
+        return (vectorA.x * vectorB.x + vectorA.y * vectorB.y + vectorA.z * vectorB.z);
+    }
 
     //SoccerPlayer FindClosestPlayerDot()
     //{
@@ -38,19 +58,40 @@ public class SoccerPlayer : MonoBehaviour
     //    return closest;
     //}
 
-    //void DrawVectors()
-    //{
-    //    foreach (SoccerPlayer other in OtherPlayers)
-    //    {
-    //        // Your code here
-    //        // ...
-    //    }
-    //}
+    void DrawVectors()
+    {
+        int i = 0;
+        foreach (SoccerPlayer other in OtherPlayers)
+        {
+            //ray to point to captain
+            Debug.DrawRay(transform.position, OtherPlayers[i].transform.position, Color.black);
 
-    //void Update()
-    //{
+            //
+            Debug.DrawRay(other.transform.position, other.OtherPlayers[i].transform.position, Color.black);
 
-    //}
+            i++;
+        }
+    }
+
+
+
+    //qns 6b part 2
+    private void ForEachLoopToFindPlayers()
+    {
+        OtherPlayers = FindObjectsOfType<SoccerPlayer>();
+        SoccerPlayer[] temp = new SoccerPlayer[OtherPlayers.Length - 1];
+        int i = 0;
+        foreach (SoccerPlayer player in OtherPlayers)
+        {
+            if (player != this)
+            {
+                temp[i] = player;
+                i++;
+            }
+        }
+
+        OtherPlayers = temp;
+    }
 }
 
 
