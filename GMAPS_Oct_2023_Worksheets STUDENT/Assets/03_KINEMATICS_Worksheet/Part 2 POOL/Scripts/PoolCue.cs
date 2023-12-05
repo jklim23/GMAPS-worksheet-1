74ca2d86b790028794f0f014ce3d3347db945378
jklim23 <junkang2233@gmail.com>
2023-12-05 09:40:17 +0800

    editing line color

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
@@ -16,35 +16,32 @@ public class PoolCue : MonoBehaviour
  		ball = ballObject.GetComponent<Ball2D>();
         
     }
-    private void FixedUpdate()
-    {
-      
-    }
+  
   
     void Update()
  	{
  		if (Input.GetMouseButtonDown(0))
  		{
- 			var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Start line drawing
+ 			var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //  line Start position 
             if (ball != null && ball.IsCollidingWith(startLinePos.x,startLinePos.y))
             {
                 //draw line start from ball and end at mouse position 
-                drawnLine = lineFactory.GetLine(ball.transform.position, startLinePos, 10f,Color.white);
-                drawnLine.EnableDrawing(true);
+                drawnLine = lineFactory.GetLine(ball.transform.position, startLinePos, 3f,Color.black);
+                drawnLine.EnableDrawing(true);// start line drawing
             }
 
 
         }
         else if (Input.GetMouseButtonUp(0) && drawnLine != null)
         {
-            drawnLine = lineFactory.GetLine(drawnLine.start, drawnLine.end, 10f, Color.white);
-            drawnLine.EnableDrawing(false);
+            drawnLine = lineFactory.GetLine(drawnLine.start, drawnLine.end, 3f, Color.black);
+            drawnLine.EnableDrawing(false);// End line drawing       
 
             //update the velocity of the white ball.
             HVector2D v = new HVector2D(-Camera.main.ScreenToWorldPoint(Input.mousePosition)-ball.transform.position);
             ball.Velocity = v;
 
-            drawnLine = null; // End line drawing            
+            drawnLine = null;      
         }
 
         if (drawnLine != null)
